0b89a0f397afb310da2b3afaaf6362fc832f9388
Faouzi Ben Makhlouf <fawzifawzi2012@gmail.com>
2021-04-29 14:31:42 +0100

    clean

--- src/app/nav/header/header.component.ts
+++ src/app/nav/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import {Router} from '@angular/router'
 import { ActivatedRoute } from '@angular/router';
@@ -10,21 +10,16 @@ import {UserService} from '../../shared/user.service'
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  private toggleButton: any;
-  private sidebarVisible: boolean;
    showMenu=false;
    menubar:any;
    role=localStorage.getItem('role')
    token=localStorage.getItem('token')
 
-  constructor(public location: Location, private element : ElementRef,private userService:UserService,private router:Router) {
-      this.sidebarVisible = false;
+  constructor(public location: Location,private userService:UserService,private router:Router) {
   }
 
 
   ngOnInit() {
-      const navbar: HTMLElement = this.element.nativeElement;
-      this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
 
   }
 
@@ -42,70 +37,4 @@ export class HeaderComponent implements OnInit {
     localStorage.clear();
     this.router.navigate(['/home'])
   }
-
-
-
-
-
-
-
-  sidebarOpen() {
-    const toggleButton = this.toggleButton;
-    const html = document.getElementsByTagName('html')[0];
-    // console.log(html);
-    // console.log(toggleButton, 'toggle');
-
-    setTimeout(function(){
-        toggleButton.classList.add('toggled');
-    }, 500);
-    html.classList.add('nav-open');
-
-    this.sidebarVisible = true;
-};
-sidebarClose() {
-    const html = document.getElementsByTagName('html')[0];
-    // console.log(html);
-    this.toggleButton.classList.remove('toggled');
-    this.sidebarVisible = false;
-    html.classList.remove('nav-open');
-};
-sidebarToggle() {
-    // const toggleButton = this.toggleButton;
-    // const body = document.getElementsByTagName('body')[0];
-    if (this.sidebarVisible === false) {
-        this.sidebarOpen();
-    } else {
-        this.sidebarClose();
-    }
-};
-isHome() {
-  var titlee = this.location.prepareExternalUrl(this.location.path());
-  if(titlee.charAt(0) === '#'){
-      titlee = titlee.slice( 1 );
-  }
-    if( titlee === '/home' ) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-isDocumentation() {
-  var titlee = this.location.prepareExternalUrl(this.location.path());
-  if(titlee.charAt(0) === '#'){
-      titlee = titlee.slice( 1 );
-  }
-    if( titlee === '/documentation' ) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-
-
-
-
 }
